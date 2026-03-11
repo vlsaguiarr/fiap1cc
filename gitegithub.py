daa060be733de4071494ece060f8e2510ae52ac1
caio.aleo@gmail.com <caio.aleo@gmail.com>
2026-03-10 21:43:20 -0300

    Aprendendo a usar Git

---
+++ gitegithub.py
@@ -0,0 +1,36 @@
+"""
+Resumo para importar repositório no Github com o Git
+
+1. Criar o repositório no Github
+
+2. Copiar o link da pasta (https://github.com/vlsaguiarr/fiap1cc.git)
+
+3. Entrar na pasta "C:\dev" ou o seu repositório pessoal 
+
+4. Clicar na opção "Open Git Bash here"
+
+5. Adicionar suas permissões:
+
+    git config --global user.name "vlsaguiarr"
+    
+    git config --global user.name caio.aleo@gmail.com
+
+6. Adicionadas as permissões, clone o repositório com o link 
+
+    git clone https://github.com/vlsaguiarr/fiap1cc.git
+
+7. Adicionar os projetos realizados na pasta clonada
+
+8. Novamente no Git Bash, mas agora na pasta clonada usaremos os códigos:
+
+    (Para tornarmos todos projetos staging)
+    git add .
+
+    (Para levar ao local repository)
+    git commit -m "Mensagem no envio"
+
+    (Para importar ao GitHub)
+    git push
+
+Pronto, todos os seus projetos estão agora no repositório do GitHub
+"""
